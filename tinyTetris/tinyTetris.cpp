61d5ff79c98857aaa552b161ace28f81a3adc216
ken54589630 <ken115ekn115@gmail.com>
2024-08-28 10:31:40 +0800

    加入專案檔案。

---
+++ tinyTetris/tinyTetris.cpp
@@ -0,0 +1,13 @@
+﻿// tinyTetris.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <ctime>
+#include <curses.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+    srand(time(0));
+    initscr();
+}
